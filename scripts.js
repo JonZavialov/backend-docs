6c516bbd51d64824a654768dd0c59d87947b5cac
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-09-26 09:38:32 -0400

    fixed nav selection bug

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 function addEvents(){
-    //TODO: Make the intro link only highlighted if there is no URL param
-    
+    if (window.location.hash === '') selectNavLink($("#navlinks p")[0]);
+
     $("#navlinks p").each((_i, item) => {
         $(item).width($("aside").width() - 50 + "px");
-        $(item).on("mousedown", () => selectNavLink($(item)));
+        $(item).on("mousedown", () => selectNavLink(item));
         $(item).on("mouseup", () => window.location.replace("/#" + $(item).attr("redirect")));
     });
 }
 
 function selectNavLink(navLink){
+    navLink = $(navLink);
     navLink.addClass("selected");
     $("#navlinks p").each((_i, item) => {
         if (item === navLink[0]) return;
