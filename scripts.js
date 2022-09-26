2b5e5411d21d2356692bfbb62b205686a3f61312
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-09-26 08:08:05 -0400

    added some writing to the docs

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function addEvents(){
     $("#navlinks p").each((_i, item) => {
-        $(item).width($("aside").width() - 50 + "px")
-        $(item).on("mousedown", () => selectNavLink($(item)))
+        $(item).width($("aside").width() - 50 + "px");
+        $(item).on("mousedown", () => selectNavLink($(item)));
         $(item).on("mouseup", () => window.location.replace("/#" + $(item).attr("redirect")));
     });
 }
